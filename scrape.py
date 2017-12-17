8e77f2800df3c71ff8825d92f144d8d3fe605b02
Thomas Propson <tcpropson@gmail.com>
2017-12-17 16:56:28 -0600

    GPS coordinate field incorporated to restaurant data

--- scrape.py
+++ scrape.py
@@ -1,16 +1,20 @@
 # scrape.py
+import config
+import json
+import googlemaps
 import requests
 from BeautifulSoup import BeautifulSoup
 
-#--Create Page URLs--
-# Each page url is composed of the 'baseURL' followed by an index value.
-baseURL = "http://www.foodnetwork.com/restaurants/shows/diners-drive-ins-and-dives/a-z/p/"
-pages = []
+#--Address to GPS Coordinates--
+gmaps = googlemaps.Client(key = config.GMAPS_API_KEY)
 
-# Hard-coding the maximum page number is a bad idea -- current maximum page number is 65. 
-# The number of pages will change over time. Find another method.
-for i in range(1,10):
-    pages.append(baseURL + str(i))
+def addressToGPS(address):
+    # 'locationData' is in JSON format.
+    locationData = gmaps.geocode(address)
+    location     = locationData[0]['geometry']['location']
+    latitude     = location['lat']
+    longitutde   = location['lng']
+    return (latitude, longitutde)
 
 #--Access and Parse Webpage-- 
 # Access time for url and html parsing is slow. One possible cause is advertisements on the foodnetwork webpages.
@@ -23,40 +27,40 @@ def makeSoup(url):
 def getRestaurantData(soup):
     # Search time for 'candidateTags' is slow.
     # One possible solution is to search for candidates within their parent tag as opposed to the entire html document.
-    candidateTags = soup.findAll('div', attrs={"class": "m-MediaBlock o-Capsule__m-MediaBlock"})
-    restaurantInfo = []
+    candidateTags  = soup.findAll('div', attrs={"class": "m-MediaBlock o-Capsule__m-MediaBlock"})
+    restaurantData = []
 
     for tag in candidateTags:
         # Only candidateTags with 'h3' header tags hold restaurant data.
         if tag.find('h3'):
-            header  = tag.find('h3', attrs={"class": "m-MediaBlock__a-Headline"})
-            name    = header.span.text
-            # The link string comes formatted: "//www...". It is formatted to "www...".
-            link    = header.a.get('href').replace("//",'')
+            header   = tag.find('h3', attrs={"class": "m-MediaBlock__a-Headline"})
+            name     = header.span.text
+            # The url string comes formatted: "//www...". It is formatted to "www...".
+            url      = header.a.get('href').replace("//",'')
             # The address string comes formatted: "...[State]&nbsp;[Zipcode]". It is formatted to "...[State] [Zipcode]".
-            address = tag.find('div', attrs={"class": "m-Info__a-Address"}).text.replace('&nbsp;',' ') 
-            restaurantInfo.append((name, link, address))
+            address  = tag.find('div', attrs={"class": "m-Info__a-Address"}).text.replace('&nbsp;',' ')
+            # Location of the restaurant given in GPS coordinates
+            location = addressToGPS(address)
+            restaurantData.append((name, address, location, url))
 
-    return restaurantInfo
+    return restaurantData
 
 # --Get All Restaurant Data--
-restaurants = []
+# Create Page URLs
+# Each page url is composed of the 'baseURL' followed by an integer index value -- currently 1 - 65 inclusive.
+baseURL = "http://www.foodnetwork.com/restaurants/shows/diners-drive-ins-and-dives/a-z/p/"
+pages   = []
+# Hard-coding the maximum page number is a bad idea.
+# The number of pages will change over time. Find another method.
+for i in range(1, 2):
+    pages.append(baseURL + str(i))
 
+restaurantData = []
 # Access, parse, and scrape each page.
 for page in pages:
-    soup         = makeSoup(page)
-    restaurants += getRestaurantData(soup)
+    soup            = makeSoup(page)
+    restaurantData += getRestaurantData(soup)
 
 # Print restaurant data.
-for restaurant in restaurants:
+for restaurant in restaurantData:
     print restaurant
-
-
-
-
-
-
-
-
-
-
